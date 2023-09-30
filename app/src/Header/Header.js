744c4d32272e4a1432abaaebb8e7142748211b6a
Igor Morita <IgorMorita17@gmail.com>
2023-09-30 00:08:42 -0300

    Update
    Criação do Header

---
+++ app/src/Header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css'
+
+function Header() {
+    return (
+        
+        <header className="main_header">
+
+            <h1 className="header_title">Sport.edu</h1>
+            
+            <ul className="header_list">
+                <li>Sobre</li>
+                <li>Participantes</li>
+                <li>Banco de Dados</li>
+            </ul>
+
+        </header>
+)}
+
+export default Header;
